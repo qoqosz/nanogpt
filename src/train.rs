c0a24310069eac786edf2c2e68be8c74db4014cd
Łukasz Bednarski <lukasz@bednarski.me>
2026-01-15 21:24:54 +0100

    Day 4

--- src/train.rs
+++ src/train.rs
@@ -1,14 +1,26 @@
+use crate::utils::multinomial_sample;
+use burn::record::CompactRecorder;
+use burn::train::metric::{AccuracyMetric, LossMetric};
+use burn::train::{Learner, SupervisedTraining};
 use burn::{
     Tensor,
     config::Config,
-    data::{dataloader::batcher::Batcher, dataset::Dataset},
+    data::{
+        dataloader::{DataLoaderBuilder, batcher::Batcher},
+        dataset::Dataset,
+    },
     module::Module,
-    nn::{Embedding, EmbeddingConfig},
+    nn::{Embedding, EmbeddingConfig, loss::CrossEntropyLossConfig},
+    optim::AdamWConfig,
     prelude::{Backend, Int},
-    tensor::TensorData,
+    tensor::{TensorData, activation::softmax, backend::AutodiffBackend, s},
+    train::{ClassificationOutput, TrainOutput, TrainStep},
 };
 use rustc_hash::{FxHashMap, FxHashSet};
+use serde::{Deserialize, Serialize};
+use std::fs;
 
+// Context window size
 static N: usize = 8;
 
 pub trait Tokenizer {
@@ -24,8 +36,11 @@ pub trait Tokenizer {
 
 #[derive(Clone)]
 pub struct Vocabulary {
+    // Actual vocabulary
     chars: FxHashSet<u8>,
+    // Char to int mapping
     stoi: FxHashMap<u8, u8>,
+    // Int to char mapping
     itos: FxHashMap<u8, u8>,
 }
 
@@ -42,6 +57,7 @@ impl Vocabulary {
         Self { chars, stoi, itos }
     }
 
+    // Number of unique chars in a vocabulary
     pub fn size(&self) -> usize {
         self.chars.len()
     }
@@ -77,14 +93,14 @@ impl Tokenizer for Vocabulary {
     }
 }
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct TextItem {
     pub x: [u8; 8],
     pub y: [u8; 8],
 }
 
 pub struct TextDataset {
-    vocabulary: Vocabulary,
+    pub vocabulary: Vocabulary,
     tokens: Vec<u8>,
 }
 
@@ -179,6 +195,66 @@ impl<B: Backend> BigramLanguageModel<B> {
     pub fn forward(&self, idx: Tensor<B, 2, Int>) -> Tensor<B, 3> {
         self.token_embedding_table.forward(idx)
     }
+
+    pub fn forward_classification(
+        &self,
+        idx: Tensor<B, 2, Int>,
+        targets: Tensor<B, 2, Int>,
+    ) -> ClassificationOutput<B> {
+        let output = self.forward(idx);
+        let [b, t, c] = output.dims();
+        let output = output.reshape([b * t, c]);
+        let targets = targets.reshape([b * t]);
+        let loss = CrossEntropyLossConfig::new()
+            .init(&output.device())
+            .forward(output.clone(), targets.clone());
+
+        ClassificationOutput::new(loss, output, targets)
+    }
+
+    pub fn generate(&self, idx: Tensor<B, 2, Int>, max_new_tokens: usize) -> Tensor<B, 2, Int> {
+        let mut idx = idx;
+        let device = idx.device();
+
+        for _ in 0..max_new_tokens {
+            // get the preditctions
+            let logits = self.forward(idx.clone());
+            // focus only on the last time step
+            let logits = logits.slice(s![.., -1, ..]).reshape([0, -1]); // becomes (B, C)
+            // apply softmax to get probabilities
+            let probs = softmax(logits, 1);
+            // sample from the distribution
+            let idx_next = probs
+                .iter_dim(0)
+                .map(|p| p.reshape([-1]))
+                .map(|p| {
+                    p.into_data()
+                        .iter()
+                        .map(|x: f32| x as f64)
+                        .collect::<Vec<f64>>()
+                })
+                .map(|p| multinomial_sample(&p, 1))
+                .map(|x| TensorData::from(x.as_slice()))
+                .map(|data| Tensor::<B, 1, Int>::from_data(data, &device))
+                .map(|tensor| tensor.reshape([1, -1]))
+                .collect();
+            let idx_next = Tensor::cat(idx_next, 0);
+            idx = Tensor::cat(vec![idx, idx_next], 1);
+        }
+
+        idx
+    }
+}
+
+impl<B: AutodiffBackend> TrainStep for BigramLanguageModel<B> {
+    type Input = TextBatch<B>;
+    type Output = ClassificationOutput<B>;
+
+    fn step(&self, item: TextBatch<B>) -> burn::train::TrainOutput<ClassificationOutput<B>> {
+        let output = self.forward_classification(item.context, item.targets);
+
+        TrainOutput::new(self, output.loss.backward(), output)
+    }
 }
 
 #[derive(Debug, Config)]
@@ -193,6 +269,67 @@ impl BigramLanguageModelConfig {
                 .init(device),
         }
     }
+}
+
+#[derive(Config, Debug)]
+pub struct BigramLanguageModelTrainingConfig {
+    pub model: BigramLanguageModelConfig,
+    pub optimizer: AdamWConfig,
+    #[config(default = 10)]
+    pub num_epochs: usize,
+    #[config(default = 64)]
+    pub batch_size: usize,
+    #[config(default = 6)]
+    pub num_workers: usize,
+    #[config(default = 43)]
+    pub seed: u64,
+    #[config(default = 1.0e-3)]
+    pub learning_rate: f64,
+}
+
+fn create_artifact_dir(artifact_dir: &str) {
+    // Remove existing artifacts before to get an accurate learner summary
+    fs::remove_dir_all(artifact_dir).ok();
+    fs::create_dir_all(artifact_dir).ok();
+}
+
+pub fn bigram_lm_train<B: AutodiffBackend<InnerBackend = B>>(
+    artifact_dir: &str,
+    config: BigramLanguageModelTrainingConfig,
+    device: B::Device,
+) {
+    create_artifact_dir(artifact_dir);
+    config
+        .save(format!("{artifact_dir}/config.json"))
+        .expect("Config should be saved successfully");
+
+    B::seed(&device, config.seed);
+
+    let text = fs::read_to_string("data/input.txt").unwrap();
+    let dataset = TextDataset::new(&text);
+
+    let batcher = TextBatcher::<B>::new(device.clone());
+    let dataloader_train = DataLoaderBuilder::new(batcher.clone())
+        .batch_size(config.batch_size)
+        .num_workers(config.num_workers)
+        .build(dataset.train());
+    let dataloader_test = DataLoaderBuilder::new(batcher)
+        .batch_size(config.batch_size)
+        .num_workers(config.num_workers)
+        .build(dataset.test());
+
+    let training = SupervisedTraining::new(artifact_dir, dataloader_train, dataloader_test)
+        .metrics((AccuracyMetric::new(), LossMetric::new()))
+        .with_file_checkpointer(CompactRecorder::new())
+        .num_epochs(config.num_epochs)
+        .summary();
+
+    let model = config.model.init::<B>(&device);
+    let result = training.launch(Learner::new(
+        model,
+        config.optimizer.init(),
+        config.learning_rate,
+    ));
 }
 
 #[cfg(test)]
---
+++ src/utils.rs
@@ -0,0 +1,11 @@
+use rand::distr::weighted::WeightedIndex;
+use rand::prelude::*;
+
+pub fn multinomial_sample(probs: &[f64], num_samples: usize) -> Vec<usize> {
+    assert!(!probs.is_empty(), "probs must not be empty");
+    // probs must be non-negative and not all zero
+    let dist = WeightedIndex::new(probs).expect("invalid probabilities");
+    let mut rng = rand::rng();
+
+    (0..num_samples).map(|_| dist.sample(&mut rng)).collect()
+}
